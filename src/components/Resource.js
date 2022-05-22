8f4f24b06f3e676012fa70a98e5f66dc33c3590a
Ewelike Emeka Nelson <emekaewelike@gmail.com>
2022-05-22 21:53:33 +0100

    third commit

--- src/components/Resource.js
+++ src/components/Resource.js
@@ -14,11 +14,11 @@ const Resource = ({user}) => {
             <div className='ml-8 mt-4 flex justify-between flex-wrap sm:flex-col'>
               <div className='border w-[49%] p-3 mb-2 sm:w-[90%]'>
                 <p className='text-sm'>full name</p>
-                <p className='text-xl font-bold'>{user.displayName}</p>
+                <p className='text-xl font-bold'>{user.displayName ? user.displayName : ''}</p>
               </div>
               <div className='border w-[49%] p-3 mb-2 sm:w-[90%]'>
                 <p className='text-sm'>email address</p>
-                <p className='text-xl font-bold'>{user.email}</p>
+                <p className='text-xl font-bold sm:text-sm'>{user.email}</p>
               </div>
               <div className='border w-[49%] p-3 mb-2 sm:w-[90%]'>
                 <p className='text-sm'>phone number</p>
